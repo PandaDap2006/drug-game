2d8c9ffabf62fc660c484d146b50a4bdec82dffc
PandaDap2006 <70108603+PandaDap2006@users.noreply.github.com>
2021-11-30 12:32:07 +0100

    dsgsdg

--- js/Player.js
+++ js/Player.js
@@ -127,5 +127,5 @@ function changePlayerTexture() {
         AnimationFrame = "idle.png";
     }
 
-    //player.style.background = "url('textures/player_" + Direction + "_" + AnimationFrame + "')"
+    player.style.background = "url('textures/player_" + Direction + "_" + AnimationFrame + "') 100%"
 }

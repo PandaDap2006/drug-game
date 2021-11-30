16a34496961821484ecc6caeac0008fb1ec282b9
PandaDap2006 <70108603+PandaDap2006@users.noreply.github.com>
2021-11-30 11:29:49 +0100

    fds

--- js/Player.js
+++ js/Player.js
@@ -127,5 +127,5 @@ function changePlayerTexture() {
         AnimationFrame = "idle.png";
     }
 
-    player.style.backgroundImage = "url('./textures/player_front_idle.png')"
+    player.style.backgroundImage = "url('../textures/player_front_idle.png')"
 }
